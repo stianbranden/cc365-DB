7bba263aba5f434e1883d6a3fb3572decee25223
Stian Branden <stianbra@elkjop.no>
2020-11-27 11:03:16 +0100

    Tableau embed working

--- routes/report.js
+++ routes/report.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 const {units} = require('../config.js')
 
 router.get('/', (req, res)=>{
+    //res.set("Content-Security-Policy", "object-src 'self' 'unsafe-eval'");
+    res.set("Content-Security-Policy", "script-src https://*.elkjop.com * blob: 'unsafe-eval' 'unsafe-inline'");
+    //"script-src * blob:"
     res.render('reporttest', {pageTitle: 'nordic'});
 });
 
