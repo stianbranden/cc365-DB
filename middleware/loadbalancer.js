d55bcd6f8285264e3c10a8a93efa5ba6bc0a3101
Stian Branden <stian@branden.no>
2021-08-13 14:25:18 +0200

    LB response complete

--- middleware/loadbalancer.js
+++ middleware/loadbalancer.js
@@ -1,9 +1,15 @@
 const {NODE_ENV} = process.env;
-const {logStd,logSys,logErr} = require('../controllers/logger');
+const {logStd} = require('../controllers/logger');
 
 module.exports = function(req, res, next){
-    if (NODE_ENV != 'production'){
-        logStd(`LoadBalancer Request from ${req.ip}`);
+    if ( req.ip === '::ffff:10.7.50.43' || req.ip === '::ffff:10.7.50.44'){
+        if (NODE_ENV != 'production'){
+            logStd(`LoadBalancer Request from ${req.ip}`);
+        }
+        res.status(200).send('Say hello to my little response')
     }
-    next();
+    else {
+        next();
+    }
+    
 }
