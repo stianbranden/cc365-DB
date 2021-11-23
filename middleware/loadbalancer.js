577f7831fbc4ac1515de35006e64cd051be92636
Stian Branden <stian@branden.no>
2021-11-23 12:03:41 +0100

    Removed logging from MW

--- middleware/loadbalancer.js
+++ middleware/loadbalancer.js
@@ -4,7 +4,7 @@ const {logStd} = require('../controllers/logger');
 module.exports = function(req, res, next){
     if ( req.ip === '::ffff:10.7.50.43' || req.ip === '::ffff:10.7.50.44'){
         if (NODE_ENV != 'production'){
-            logStd(`LoadBalancer Request from ${req.ip}`);
+            //logStd(`LoadBalancer Request from ${req.ip}`);
         }
         res.status(200).send('Say hello to my little response')
     }
