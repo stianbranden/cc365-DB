8dbe3769f9c1a047e8c42144da97a3bad62eb74a
Stian Branden <stian@branden.no>
2021-12-22 16:05:13 +0100

    pm2 script

---
+++ controllers/getPm2.js
@@ -0,0 +1,45 @@
+const {logStd, logTab, logErr} = require('./logger');
+const pm2 = require('pm2');
+const moment = require('moment')
+
+const getPm2Data = async _=>{
+    pm2.connect(err=>{
+        const processes = []
+        if (err) logErr(err);
+        else {
+            pm2.list(async (err, list)=>{
+                if (err) logErr(err);
+                else {
+                    list.forEach(async p=>{
+                        //logStd(JSON.stringify(p))
+                        //const data = await getProcessData(p.pid);
+                        processes.push({
+                            pid: p.pid, 
+                            name: p.name, 
+                            memory: p.monit.memory, 
+                            cpu: p.monit.cpu, 
+                            status: p.pm2_env.status,
+                            uptime: moment(p.pm2_env.pm2_uptime).format(),
+                            restarts: p.pm2_env.unstable_restarts
+                        })
+                    });
+                }
+                logTab(processes, 'Process-list')
+                pm2.disconnect();
+            })
+        }
+    });
+}
+
+const getProcessData = pid =>{
+    return new Promise((resolve, reject)=>{
+        pm2.describe(pid, (err, data)=>{
+            logStd(JSON.stringify(data));
+            resolve(data)
+        })
+    })
+    
+}
+
+getPm2Data();
+//getProcessData("local")
